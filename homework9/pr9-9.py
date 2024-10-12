1b2826d88eb61970ed4381da9a19cd7149dafdd9
David44ru <Peresypkin.dovid@gmail.com>
2024-10-12 13:42:23 +0500

    добавили функцию для парсинга email-ов и так же цикл для поатора программы

--- homework9/pr9-9.py
+++ homework9/pr9-9.py
@@ -3,3 +3,18 @@ import re
 # Регулярное выражение для разделения email на имя пользователя и домен
 email_pattern = r"^([a-zA-Z0-9_.+-]+)@([a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)$"
 
+# Функция для парсинга email
+def parse_email(email):
+    match = re.match(email_pattern, email)
+    if match:
+        username, domain = match.groups()
+        return username, domain
+    else:
+        return None
+
+# Цикл для повтора программы
+while True:
+    # Ввод email пользователя
+    email = input("Введите ваш email: ")
+
+    
