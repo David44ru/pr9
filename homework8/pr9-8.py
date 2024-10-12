664e7d5002189cbe897839b8bd39f312975c66fe
David44ru <Peresypkin.dovid@gmail.com>
2024-10-12 13:20:28 +0500

    добавили функцию для выбора чисел пользователем

--- homework8/pr9-8.py
+++ homework8/pr9-8.py
@@ -1,6 +1,5 @@
 import random
 
-# Лотерейный билет (список списков)
 ticket = [
     [1, 2, 3, 4, 5],
     [6, 7, 8, 9, 10],
@@ -8,3 +7,18 @@ ticket = [
     [16, 17, 18, 19, 20],
     [21, 22, 23, 24, 25],
 ]
+
+def user_selection(ticket):
+    user_numbers = []
+    for row in ticket:
+        while True:
+            try:
+                number = int(input(f"Выберите число из ряда {row}: "))
+                if number in row:
+                    user_numbers.append(number)
+                    break
+                else:
+                    print("Число должно быть из указанного ряда. Попробуйте еще раз.")
+            except ValueError:
+                print("Пожалуйста, введите правильное число.")
+    return user_numbers
